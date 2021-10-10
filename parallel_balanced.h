1346e40c6ac9fbd772e865a8c625beb1ade9dc53
Dymitr <dymitrlubczyk@gmail.com>
2021-10-10 16:00:09 +0200

    Change function signature

--- parallel_balanced.h
+++ parallel_balanced.h
@@ -2,7 +2,7 @@
 void master(int N, int R, int node_count);
 void worker(int N, int R, int node_count, int id);
 int get_stop();
-int send_stop(int stop, int node_count);
+int send_stop(int node_count);
 int get_results(int node_count);
 void send_result(int result);
 int *get_task(int task_size);
