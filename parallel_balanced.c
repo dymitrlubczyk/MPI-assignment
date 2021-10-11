830485a36519e2af9ab08587fdf24606e6aa834a
Dymitr <dymitrlubczyk@gmail.com>
2021-10-11 16:30:52 +0200

    Add missing argument

--- parallel_balanced.c
+++ parallel_balanced.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &node_count);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-    id == 0 ? master(N, R, node_count) : worker(N, R, node_count, id);
+    id == 0 ? master(N, R, node_count, init_mode) : worker(N, R, node_count, id);
 
     MPI_Finalize();
 
