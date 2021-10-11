f0f8c2d704578519dba4b1f52d9a35f25dd3dfb4
Dymitr <dymitrlubczyk@gmail.com>
2021-10-11 19:31:49 +0200

    Debbuging

--- parallel_imbalanced.c
+++ parallel_imbalanced.c
@@ -46,7 +46,7 @@ void master(int node_count, char init_mode)
     {
         for (int i = 0; i < TASK_SIZE && counter < R; ++i)
         {
-            next_task = distribute_work(A, tasks_count, next_task, node_count);
+            next_task = distribute_work(work_requests, A, tasks_count, next_task, node_count);
             counter += test_imbalanced(A[TASK_SIZE * my_task + i]);
             counter += get_results(result_requests, node_count);
         }
