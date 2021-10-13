3cf8a15f4799c53edfb336e515acb03da55b3760
Dymitr <dymitrlubczyk@gmail.com>
2021-10-13 21:43:03 +0200

    Debugging

--- parallel_imbalanced.c
+++ parallel_imbalanced.c
@@ -46,6 +46,7 @@ void master(int node_count, char init_mode)
     {
         for (int i = 0; i < TASK_SIZE && counter < R; ++i)
         {
+            printf("index to be accessed: %d", TASK_SIZE * my_task + i);
             next_task = distribute_work(work_requests, A, tasks_count, next_task, node_count);
             counter += test_imbalanced(A[TASK_SIZE * my_task + i]);
             counter += get_results(result_requests, node_count);
