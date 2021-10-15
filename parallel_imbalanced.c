a1d58706c5b9f85e28f470fd1f3d8541568f13a4
Dymitr <dymitrlubczyk@gmail.com>
2021-10-15 13:49:48 +0200

    Debuigging

--- parallel_imbalanced.c
+++ parallel_imbalanced.c
@@ -181,11 +181,12 @@ void send_stop(int node)
 
 void send_task(int node, int task, int *A, MPI_Request *work_request)
 {
+    int result;
+    MPI_Irecv(&result, 1, MPI_INT, node, WORK_TAG, MPI_COMM_WORLD, work_request);
+    
     printf("Sending task %d to %d\n", task, node);
     MPI_Send(&A[task * TASK_SIZE], TASK_SIZE, MPI_INT, node, WORK_TAG, MPI_COMM_WORLD);
     printf("Done\n");
-    int result;
-    MPI_Irecv(&result, 1, MPI_INT, node, WORK_TAG, MPI_COMM_WORLD, work_request);
 }
 
 MPI_Request *initialise_requests(int node_count, int tag)
