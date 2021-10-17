bf00b0b844bf0ec9d5ba54887ee353699af9c38e
Dymitr <dymitrlubczyk@gmail.com>
2021-10-17 19:23:29 +0200

    Debugging

--- parallel_imbalanced.c
+++ parallel_imbalanced.c
@@ -37,17 +37,17 @@ void master(int node_count, char init_mode)
     int tasks_count = N / TASK_SIZE;
 
     int *A = initialise(init_mode);
-    
+
     MPI_Request result_request;
     int result;
     MPI_Irecv(&result, 1, MPI_INT, MPI_ANY_SOURCE, RESULT_TAG, MPI_COMM_WORLD, &result_request);
-    
+
     MPI_Request work_request;
     int work_result;
     MPI_Irecv(&work_result, 1, MPI_INT, MPI_ANY_SOURCE, WORK_TAG, MPI_COMM_WORLD, &work_request);
 
     double start = MPI_Wtime();
-    
+
     while (counter < R && my_task < tasks_count)
     {
         for (int i = 0; i < TASK_SIZE && counter < R; ++i)
@@ -59,13 +59,12 @@ void master(int node_count, char init_mode)
 
         printf("Counter: %d, done %d/%d\n", counter, next_task, tasks_count);
         my_task = next_task++;
-
     }
 
     finish(&result_request, &work_request, A, tasks_count, next_task, node_count);
+    
     double end = MPI_Wtime();
-
-     printf("Execution time: %fs\n", end - start);
+    printf("Execution time: %fs\n", end - start);
 }
 
 void worker(int node_count, int id)
@@ -87,44 +86,39 @@ void worker(int node_count, int id)
             stop = get_stop(stop, stop_request);
             send_result(stop, result);
         }
-        
+
         free(task);
-       // stop = get_stop(stop, stop_request);
     }
 }
 
 void send_result(int stop, int result)
 {
     if (!stop && result)
-    {
         MPI_Send(&result, 1, MPI_INT, 0, RESULT_TAG, MPI_COMM_WORLD);
-       // printf("Result sent\n");
-    }
 }
 
 int get_results(MPI_Request *result_request, int node_count)
 {
-    int result = 0;
-    int ready = 1; 
+    int ready = 1;
     int counter = 0;
 
-    while(ready)
+    while (ready)
     {
         MPI_Status status;
-
-      //  printf("Looking for results\n");
         MPI_Test(result_request, &ready, &status);
 
-        if (ready && status.MPI_SOURCE>0 && status.MPI_SOURCE<node_count)
+        if (ready && status.MPI_SOURCE > 0 && status.MPI_SOURCE < node_count)
         {
-           // printf("Result ready, Source: %d, Tag: %d\n", status.MPI_SOURCE, status.MPI_TAG);
-            counter += 1;
+            counter++;
+
+            int result;
             MPI_Request new_request;
             MPI_Irecv(&result, 1, MPI_INT, MPI_ANY_SOURCE, RESULT_TAG, MPI_COMM_WORLD, &new_request);
+            
             *result_request = new_request;
         }
 
-        else 
+        else
             ready = 0;
     }
 
@@ -134,23 +128,22 @@ int get_results(MPI_Request *result_request, int node_count)
 int distribute_work(MPI_Request *work_request, int *A, int tasks_count, int next_task, int node_count)
 {
     int requested = 1;
-    
-    while(requested)
+
+    while (requested)
     {
         requested = 0;
         MPI_Status status;
-
-      //  printf("Looking for work requests\n");
         MPI_Test(work_request, &requested, &status);
 
-        if (requested && status.MPI_SOURCE>0 && status.MPI_SOURCE<node_count){
-           // printf("Asking for work, Source: %d, Tag: %d\n", status.MPI_SOURCE, status.MPI_TAG);
+        if (requested && status.MPI_SOURCE > 0 && status.MPI_SOURCE < node_count)
+        {
             int task = next_task < tasks_count ? next_task++ : 0;
             send_task(status.MPI_SOURCE, task, A);
-            
-            MPI_Request new_request;
+
             int result;
+            MPI_Request new_request;
             MPI_Irecv(&result, 1, MPI_INT, MPI_ANY_SOURCE, WORK_TAG, MPI_COMM_WORLD, &new_request);
+            
             *work_request = new_request;
         }
     }
@@ -160,60 +153,44 @@ int distribute_work(MPI_Request *work_request, int *A, int tasks_count, int next
 
 int get_stop(int stop, MPI_Request stop_request)
 {
-    if(stop) return 1;
+    if (!stop)
+        MPI_Test(&stop_request, &stop, MPI_STATUS_IGNORE);
 
-    MPI_Test(&stop_request, &stop, MPI_STATUS_IGNORE);
-    // if(stop) printf("Got stop\n");
     return stop;
 }
 
 int *get_task()
 {
-    int ready = 1;
+    int ready;
     MPI_Request work_request;
-
     MPI_Isend(&ready, 1, MPI_INT, 0, WORK_TAG, MPI_COMM_WORLD, &work_request);
 
     int *task = allocate_mem(TASK_SIZE);
-
     MPI_Recv(task, TASK_SIZE, MPI_INT, 0, WORK_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     MPI_Wait(&work_request, MPI_STATUS_IGNORE);
 
-    // printf("Task received, first in buffer %d\n", task[0]);
-
     return task;
-}
-
-void send_stop(int node)
-{
-    int stop = 1;
-
-    MPI_Request stop_request;
-
-    MPI_Isend(&stop, 1, MPI_INT, node, STOP_TAG, MPI_COMM_WORLD, &stop_request);
 }
 
 void send_task(int node, int task, int *A)
 {
-   // printf("Sending task %d to %d\n", task, node);
     MPI_Send(&A[task * TASK_SIZE], TASK_SIZE, MPI_INT, node, WORK_TAG, MPI_COMM_WORLD);
-
 }
 
-void finish(MPI_Request *result_request, MPI_Request *work_request, int *A, int tasks_count, int next_task, int node_count){
+void finish(MPI_Request *result_request, MPI_Request *work_request, int *A, int tasks_count, int next_task, int node_count)
+{
 
-    for (int i = 1; i < node_count; ++i){
+    for (int i = 1; i < node_count; ++i)
+    {
         int stop = 1;
         MPI_Request stop_request;
         MPI_Isend(&stop, 1, MPI_INT, i, STOP_TAG, MPI_COMM_WORLD, &stop_request);
-
     }
 
     get_results(result_request, node_count);
     distribute_work(work_request, A, tasks_count, next_task, node_count);
     free(A);
-
 }
 
 int *initialise(char init_mode)
--- parallel_imbalanced.h
+++ parallel_imbalanced.h
@@ -7,7 +7,6 @@ int get_results(MPI_Request *result_requests, int node_count);
 int distribute_work(MPI_Request *work_request, int *A, int tasks_count, int next_task, int node_count);
 int get_stop(int stop, MPI_Request stop_request);
 int* get_task();
-void send_stop(int node);
 void send_task(int node, int task, int *A);
 int *initialise(char init_mode);
 void finish(MPI_Request *result_requests, MPI_Request *work_requests, int *A, int tasks_count, int next_task, int node_count);
